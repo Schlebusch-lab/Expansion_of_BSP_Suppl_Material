16ba436526f058569adf24d17c67df0d163b982b
cesarforteslima <fortes.con.o@gmail.com>
2023-04-05 16:24:49 +0200

    Update Plotting_script.sh

--- Suppl_Material_bioRxiv_V1/scripts/Plotting_script.sh
+++ Suppl_Material_bioRxiv_V1/scripts/Plotting_script.sh
@@ -25,10 +25,10 @@ for DB in Only-African_Populations; do bokeh -i Patterns/Map_${DB}_df.csv -o ${F
 title="Geographical locations of Bantu-speaking populations"
 
 add_info="Figure showing geographical locations all the groups included in the Only-BSP dataset."
-for DB in Only-BSP_Groups; do bokeh -i Patterns/Map_${DB}_df.csv -o ${Folder}/Fig.S_1.5a_Map -t "$title" -a "$add_info"; done
+for DB in Only-BSP_Groups; do bokeh -i Patterns/Map_${DB}_df.csv -o ${Folder}/Fig.S_1.5b_Map -t "$title" -a "$add_info"; done
 
 add_info="Figure showing geographical locations all the populations included in the Only-BSP dataset."
-for DB in Only-BSP_Populations; do bokeh -i Patterns/Map_${DB}_df.csv -o ${Folder}/Fig.S_1.5c_Map -t "$title" -a "$add_info"; done
+for DB in Only-BSP_Populations; do bokeh -i Patterns/Map_${DB}_df.csv -o ${Folder}/Fig.S_1.5d_Map -t "$title" -a "$add_info"; done
 
 
 
